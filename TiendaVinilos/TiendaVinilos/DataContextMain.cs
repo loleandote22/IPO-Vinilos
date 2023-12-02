25cb5d6c3f1b84b1103a883d40379cb54b034445
loleandote <117114417+loleandote22@users.noreply.github.com>
2023-12-02 21:56:58 +0100

    Fin ventana Cliente sin mensaje pomociones

--- TiendaVinilos/TiendaVinilos/DataContextMain.cs
+++ TiendaVinilos/TiendaVinilos/DataContextMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI;
 using System.Windows;
 using System.Windows.Input;
 
@@ -10,27 +11,32 @@ namespace TiendaVinilos
 {
     internal class DataContextMain: ViewModelBase
     {
-        private ICommand command;
         private String texto;
         private String idiomaSeleccionado;
-        public ICommand Command { get => command; set => command = value; }
         private ICommand commandArtistas;
         private ICommand commandDiscos;
         private ICommand commandPromociones;
         private ICommand commandContacto;
+        private ICommand commandCompra;
+        private ICommand usuarioCommand;
+        private ICommand inicioCommand;
         private List<String> listaIdiomas = new List<string> { "Español", "Inglés" };
         private int elementosCarrito;
+        private Page pantalla;
         private String titulo;
        // private 
         public DataContextMain()
         {
-            Command= new RelayCommand(new Action<object>((o) => hacerAlgo()));
             commandArtistas= new RelayCommand(new Action<object>((o) => verArtistas()));
             commandDiscos= new RelayCommand(new Action<object>((o) => verDiscos()));
             commandPromociones = new RelayCommand(new Action<object>((o) => verPromociones()));
             commandContacto =new RelayCommand(new Action<object>((o) => verContacto()));
+            commandCompra = new RelayCommand(new Action<object>((o) => verCompra()));
+            usuarioCommand= new RelayCommand(new Action<object>((o) => verPerfil()));
+            inicioCommand = new RelayCommand(new Action<object>((o) => verInicio()));
             elementosCarrito = 0;
             IdiomaSeleccionado = listaIdiomas[0];
+            Titulo = "Bienvenido";
             DateTime fecha = System.DateTime.Now;
         }
 
@@ -49,17 +55,24 @@ namespace TiendaVinilos
         public ICommand CommandDiscos { get => commandDiscos; set => commandDiscos = value; }
         public ICommand CommandPromociones { get => commandPromociones; set => commandPromociones = value; }
         public ICommand CommandContacto { get => commandContacto; set => commandContacto = value; }
-        public int ElementosCarrito { get => elementosCarrito; set=> elementosCarrito = value; }
+        public ICommand CommandCompra { get => commandCompra; set => commandCompra = value; }
+        public ICommand UsuarioCommand { get => usuarioCommand; set => usuarioCommand = value; }
+        public ICommand InicioCommand { get => inicioCommand; set => inicioCommand = value; }
+        public int ElementosCarrito { get => elementosCarrito; set { elementosCarrito = value;OnPropertyChanged("ElementosCarrito"); } }
         public String Titulo { get => titulo;set=>titulo = value; }
-       // public DateTime Fecha { get=> }
+        public Page Pantalla { get => pantalla; set {
+                pantalla = value;
+                OnPropertyChanged("Pantalla");
+            } }
 
-        private void hacerAlgo()
-        {
-            MessageBox.Show("Hola");
-        }
+
+        // public DateTime Fecha { get=> }
         private void verArtistas() { MessageBox.Show("Artistas"); }
         private void verDiscos() { MessageBox.Show("Discos"); }
         private void verPromociones() { MessageBox.Show("Promociones"); }
         private void verContacto() { MessageBox.Show("Contacto"); }
+        private void verCompra() { MessageBox.Show("Compra"); }
+        private void verPerfil() { /*MessageBox.Show("Perfil"); */ElementosCarrito += 1; }
+        private void verInicio() { MessageBox.Show("Inicio"); }
     }
 }
