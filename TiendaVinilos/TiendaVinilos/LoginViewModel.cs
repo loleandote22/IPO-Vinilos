f4030673ec8afd76cff2a643814e43d3b3ccc0ea
loleandote <117114417+loleandote22@users.noreply.github.com>
2023-12-07 14:19:16 +0100

    Actualizacion Login

---
+++ TiendaVinilos/TiendaVinilos/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TiendaVinilos
+{
+    internal class LoginViewModel:ViewModelBase
+    {
+        public LoginViewModel()
+        {
+            AccederCommand = new RelayCommand(new Action<object>((o) => acceder()));
+        }
+        private ICommand accederCommand;
+        public ICommand AccederCommand { get => accederCommand; set => accederCommand = value; }
+        private void acceder()
+        {
+            MainWindowAdministrador ventana = new MainWindowAdministrador();
+                    ventana.Show();
+            //    if (usuario.tipo == "Administrador")
+            //    {
+            //        MainWindowAdministrador ventana = new MainWindowAdministrador();
+            //        ventana.Show();
+            //    }
+            //    else
+            //    {
+            //        MainWindowCliente ventana = new MainWindowCliente();
+            //        ventana.show();
+            //    }
+            //}
+        }
+    }
+}
